6adbc4dde0d3cca254abe8e2d9cc72cedfe16f3c
ilcyb <hybmail1996@gmail.com>
2020-10-14 18:44:38 +0800

    server attack

---
+++ src/serverattack_main.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+
+import os
+import copy
+import time
+import pickle
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from tensorboardX import SummaryWriter
+import torchvision.utils as vutils
+
+from options import args_parser
+from update import LocalUpdate, test_inference, AdversaryGanUpdateMnist, AdversaryUpdate
+from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar, DCGANDiscriminator_mnist, DCGANGenerator_mnist
+from utils import get_dataset, average_weights, exp_details, get_dataset_ganattack, get_dataset_split_by_label, get_dataset_idxgroup_ganattack
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    # define paths
+    path_project = os.path.abspath('..')
+    logger = SummaryWriter('./logs')
+
+    args = args_parser()
+    exp_details(args)
+
+    if args.gpu:
+        torch.cuda.set_device('cuda:{}'.format(args.gpu))
+    device = 'cuda' if args.gpu else 'cpu'
+
+    # load dataset and user groups
+    if args.model == 'dcgan':
+        _, _, user_groups = get_dataset(args)
+        train_dataset, test_dataset, label_indexs = get_dataset_split_by_label(args)
+    else:
+        train_dataset, test_dataset, user_groups = get_dataset(args)
+    global_model = None
+
+    # BUILD MODEL
+    if args.model == 'cnn':
+        # Convolutional neural netork
+        if args.dataset == 'mnist':
+            global_model = CNNMnist(args=args)
+        elif args.dataset == 'fmnist':
+            global_model = CNNFashion_Mnist(args=args)
+        elif args.dataset == 'cifar':
+            global_model = CNNCifar(args=args)
+
+    elif args.model == 'mlp':
+        # Multi-layer preceptron
+        img_size = train_dataset[0][0].shape
+        len_in = 1
+        for x in img_size:
+            len_in *= x
+            global_model = MLP(dim_in=len_in, dim_hidden=64,
+                               dim_out=args.num_classes)
+
+    elif args.model == 'dcgan':
+        # deep convolutional generative adversarial networks
+        if args.dataset == 'mnist':
+            global_model = DCGANDiscriminator_mnist(args=args)
+        else:
+            # TODO add datasets support
+            exit('Error: unrecognized dataset')
+    else:
+        exit('Error: unrecognized model')
+
+    # Set the model to train and send it to device.
+    global_model.to(device)
+    global_model.train()
+
+    # copy weights
+    global_weights = global_model.state_dict()
+
+    # Training
+    train_loss, train_accuracy = [], []
+    val_acc_list, net_list = [], []
+    cv_loss, cv_acc = [], []
+    print_every = 2
+    val_loss_pre, counter = 0, 0
+
+    # adversary model
+    if args.model == 'dcgan' and args.dataset == 'mnist':
+        generator_model = DCGANGenerator_mnist(args=args)
+        adversary_gan_update = AdversaryGanUpdateMnist(copy.deepcopy(global_model), generator_model, 
+                                                args, logger, args.wanted_label_index, false_label_index=10)
+    elif args.model == 'dcgan' and args.dataset == 'cifar':
+        pass
+
+    for epoch in tqdm(range(args.epochs)):
+        local_weights, local_losses = [], []
+        idx_group = get_dataset_idxgroup_ganattack(args, [[0,1,2,3,4,5], [6,7,8,9]], label_indexs)
+        print(f'\n | Global Training Round : {epoch+1} |\n')
+
+        global_model.train()
+        m = max(int(args.frac * args.num_users), 1)
+        # 从总用户中随机抽取需要的用户
+        idxs_users = np.random.choice(range(0, args.num_users), m, replace=False)
+
+        data_idx = 0
+        for idx in idxs_users:
+            # TODO 不应该每一轮都新建一个Update类
+            global_model_copy = copy.deepcopy(global_model)
+            local_model = LocalUpdate(args=args, dataset=train_dataset,
+                                        idxs=idx_group[data_idx], logger=logger)
+            w, loss = local_model.update_weights(
+                model=global_model_copy, global_round=epoch)
+            local_weights.append(copy.deepcopy(w))
+            local_losses.append(copy.deepcopy(loss))
+            data_idx += 1
+
+        # 服务器进行攻击
+        global_model_copy = copy.deepcopy(global_model)
+        server_adversary = AdversaryUpdate(args=args, dataset=train_dataset,
+                                        idxs=[], logger=logger,
+                                        adversary_gan_update=adversary_gan_update,
+                                        discriminator_model=global_model_copy)
+        server_adversary.train_generator()
+        w = server_adversary.update_weights(
+            model=global_model_copy, global_round=epoch)
+        local_weights.append(copy.deepcopy(w))
+
+        # update global weights
+        global_weights = average_weights(local_weights)
+
+        # update global weights
+        global_model.load_state_dict(global_weights)
+
+        loss_avg = sum(local_losses) / len(local_losses)
+        train_loss.append(loss_avg)
+
+        # Calculate avg training accuracy over all users at every epoch
+        list_acc, list_loss = [], []
+        global_model.eval()
+        # print('test idx:{}'.format(idx))
+        for c in range(args.num_users):
+            # FIXME
+            # 这里的user_groups[idx]是否应该是user_groups[c]?
+            local_model = LocalUpdate(args=args, dataset=train_dataset,
+                                      idxs=user_groups[c], logger=logger)
+            acc, loss = local_model.inference(model=global_model)
+            list_acc.append(acc)
+            list_loss.append(loss)
+        train_accuracy.append(sum(list_acc)/len(list_acc))
+
+        # print global training loss after every 'i' rounds
+        if (epoch+1) % print_every == 0:
+            print(f' \nAvg Training Stats after {epoch+1} global rounds:')
+            print(f'Training Loss : {np.mean(np.array(train_loss))}')
+            print('Train Accuracy: {:.2f}% \n'.format(100*train_accuracy[-1]))
+
+        # save generated fake images each epoch
+        randz = torch.randn(1, 100, 1, 1, device=device)
+        generated_fake_image = generator_model(randz).to('cpu').detach()
+        vutils.save_image(generated_fake_image, './generated_fake_datas/epoch_{}.png'.format(epoch))
+
+    # Test inference after completion of training
+    test_acc, test_loss = test_inference(args, global_model, test_dataset)
+
+    print(f' \n Results after {args.epochs} global rounds of training:')
+    print("|---- Avg Train Accuracy: {:.2f}%".format(100*train_accuracy[-1]))
+    print("|---- Test Accuracy: {:.2f}%".format(100*test_acc))
+
+    # Saving the objects train_loss and train_accuracy:
+    file_name = './save/objects/{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}].pkl'.\
+        format(args.dataset, args.model, args.epochs, args.frac, args.iid,
+               args.local_ep, args.local_bs)
+
+    with open(file_name, 'wb') as f:
+        pickle.dump([train_loss, train_accuracy], f)
+
+    print('\n Total Run Time: {0:0.4f}'.format(time.time()-start_time))
+
+    # PLOTTING (optional)
+    # import matplotlib
+    # import matplotlib.pyplot as plt
+    # matplotlib.use('Agg')
+
+    # Plot Loss curve
+    # plt.figure()
+    # plt.title('Training Loss vs Communication rounds')
+    # plt.plot(range(len(train_loss)), train_loss, color='r')
+    # plt.ylabel('Training loss')
+    # plt.xlabel('Communication Rounds')
+    # plt.savefig('./save/fed_{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}]_loss.png'.
+    #             format(args.dataset, args.model, args.epochs, args.frac,
+    #                    args.iid, args.local_ep, args.local_bs))
+    #
+    # # Plot Average Accuracy vs Communication rounds
+    # plt.figure()
+    # plt.title('Average Accuracy vs Communication rounds')
+    # plt.plot(range(len(train_accuracy)), train_accuracy, color='k')
+    # plt.ylabel('Average Accuracy')
+    # plt.xlabel('Communication Rounds')
+    # plt.savefig('./save/fed_{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}]_acc.png'.
+    #             format(args.dataset, args.model, args.epochs, args.frac,
+    #                    args.iid, args.local_ep, args.local_bs))
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ from torchvision import datasets, transforms
 from sampling import mnist_iid, mnist_noniid, mnist_noniid_unequal
 from sampling import cifar_iid, cifar_noniid
 import torchvision.utils as vutils
+import math
+import numpy as np
 
 
 def get_dataset(args):
@@ -72,13 +74,83 @@ def get_dataset(args):
 
     return train_dataset, test_dataset, user_groups
 
-# def add_data_to_dataset(dataset, new_classes, new_data, new_label):
-#     added_data_index_start = dataset.__len__()
-#     dataset.classes.extend(new_classes)
-#     dataset.data = torch.cat((dataset.data, new_data.to('cpu')))
-#     dataset.targets = torch.cat((dataset.targets, new_label.to('cpu')))
-#     added_data_index_end = dataset.__len__()
-#     return dataset, [i for i in range(added_data_index_start, added_data_index_end)]
+# 根据label将数据集整体划分为一个dict[key=label,value=dataset]
+def get_dataset_split_by_label(args):
+    if args.dataset == 'cifar':
+        pass
+
+    elif args.dataset == 'mnist' or 'fmnist':
+        if args.dataset == 'mnist':
+            data_dir = './data/mnist/'
+        else:
+            data_dir = './data/fmnist/'
+
+        apply_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))])
+
+        train_dataset = datasets.MNIST(data_dir, train=True, download=True,
+                                       transform=apply_transform)
+
+        test_dataset = datasets.MNIST(data_dir, train=False, download=True,
+                                      transform=apply_transform)
+
+        digit_indexs = {}
+        for digit in range(10):
+            idx = train_dataset.train_labels == digit
+            digit_indexs[digit] = [i for i in range(len(idx)) if idx[i]==True]
+        
+        return train_dataset, test_dataset, digit_indexs
+
+# 将给定的多个数据集合成一个数据集
+def merge_multidatasets(*datasets):
+    total_dataset = datasets[0]
+    total_data = torch.cat([dataset.train_data for dataset in datasets] ,0)
+    total_label = torch.cat([dataset.train_labels for dataset in datasets], 0)
+    total_dataset.train_labels = total_label
+    total_dataset.train_data = total_data
+    return total_dataset
+
+# 根据传入的label分组返回对应的datasets分组
+def get_dataset_ganattack(args, label_split):
+    train_dataset, test_dataset, label_indexs = get_dataset_split_by_label(args)
+    local_data_fraction = len(label_indexs)/args.num_users # 为什么本地数据集对总数据集的占比是（分类数量/总人数）而不是（1/总人数）？因为数据集大小已经被分类数量给稀释了一次了
+    # idx_groups [[labels1的数据的idxs],[labels2的数据的idxs],...]
+    idx_groups = []
+    if local_data_fraction>1:
+        local_data_fraction = 1
+    for labels in label_split:
+        indexs = []
+        for label in labels:
+            indexs +=  list(np.random.choice(label_indexs[label], (int)(len(label_indexs[label])*local_data_fraction), replace=False))
+        idx_groups.append(indexs)
+    return train_dataset, test_dataset, idx_groups
+
+# 根据传入的label分组返回随机选取的数据
+def get_dataset_idxgroup_ganattack(args, label_split, label_indexs):
+    local_data_fraction = len(label_indexs)/args.num_users # 为什么本地数据集对总数据集的占比是（分类数量/总人数）而不是（1/总人数）？因为数据集大小已经被分类数量给稀释了一次了
+    # idx_groups [[labels1的数据的idxs],[labels2的数据的idxs],...]
+    idx_groups = []
+    if local_data_fraction>1:
+        local_data_fraction = 1
+    for labels in label_split:
+        indexs = []
+        for label in labels:
+            indexs +=  list(np.random.choice(label_indexs[label], (int)(len(label_indexs[label])*local_data_fraction), replace=False))
+        idx_groups.append(indexs)
+    return idx_groups
+
+# # C(n, m)
+# def cob(n, m):
+#     return math.factorial(n)//(math.factorial(n-m)*math.factorial(m)) 
+
+# # 将给定的组件组合成required_nums个不同的组合
+# def combination(components, required_nums):
+#     max_combination_nums = 0
+#     components_len = len(components)
+#     for i in range(1, components_len+1):
+        
+    
 
 def average_weights(w):
     """
