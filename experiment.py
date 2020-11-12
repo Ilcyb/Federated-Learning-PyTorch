bf57af5852a3d56df3c27456a860cb8525a51b64
ilcyb <hybmail1996@gmail.com>
2020-11-12 19:34:38 +0800

    experiment

--- experiment.py
+++ experiment.py
@@ -5,6 +5,7 @@ import subprocess
 # label gan_lr gan_epoch optim_method
 local_gan_epoch = 25
 local_epoch = 1
+experiment_name = 'server_encypt_attack_noniid'
 for label in range(10):
     for gan_lr in [0.0002, 0.002]:
-                subprocess.call('python src/serverattack_main.py --mode=production --model=dcgan --dataset=mnist --gpu=0 --epochs=100 --num_users=200 --frac=0.2 --local_ep={} --wanted_label_index={} --local_gan_epoch={} --local_gan_lr={}'.format(local_epoch, label, local_gan_epoch, gan_lr), shell=True)
+                subprocess.call('python src/serverattack_main.py --mode=production --model=dcgan --dataset=mnist --gpu=0 --epochs=100 --num_users=200 --frac=0.2 --local_ep={} --wanted_label_index={} --local_gan_epoch={} --local_gan_lr={} --experiment_name={}'.format(local_epoch, label, local_gan_epoch, gan_lr, experiment_name), shell=True)
