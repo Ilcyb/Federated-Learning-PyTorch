6dab9d47de06ede357f84f4175efb96ca7a0299d
ilcyb <hybmail1996@gmail.com>
2020-11-13 13:48:01 +0800

    fix

--- experiment.py
+++ experiment.py
@@ -8,4 +8,4 @@ local_epoch = 1
 experiment_name = 'server_encypt_attack_noniid'
 for label in range(10):
     for gan_lr in [0.0002, 0.002]:
-                subprocess.call('python src/serverattack_main.py --mode=production --model=dcgan --dataset=mnist --gpu=0 --epochs=100 --num_users=200 --frac=0.2 --local_ep={} --wanted_label_index={} --local_gan_epoch={} --local_gan_lr={} --experiment_name={}'.format(local_epoch, label, local_gan_epoch, gan_lr, experiment_name), shell=True)
+                subprocess.call('python src/serverattack_main.py --mode=production --model=dcgan --dataset=mnist --gpu=0 --epochs=100 --num_users=100 --frac=0.1 --local_ep={} --wanted_label_index={} --local_gan_epoch={} --local_gan_lr={} --experiment_name={}'.format(local_epoch, label, local_gan_epoch, gan_lr, experiment_name), shell=True)
